ab45f6800d1bc00d108da5962ad115e1263f321e
chanpi1222 <chanpi1222@gmail.com>
2020-07-25 18:54:44 +0900

    a
    git merge --abort

---
+++ app/models/user.rb
@@ -0,0 +1,19 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+<<<<<<< Updated upstream
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable,  :validatable
+
+  validates :name, presence: true, uniqueness: true
+
+end
+=======
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  validates :name, presence: true, uniqueness: true
+
+end
+>>>>>>> Stashed changes
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+<<<<<<< Updated upstream
+  devise_for :users 
+  
+=======
+  devise_for :users
+
+>>>>>>> Stashed changes
   root "messages#index"
+  resources :users, only: [:edit, :update]
 end
